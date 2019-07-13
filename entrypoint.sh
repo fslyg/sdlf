c24ba3f2d8967545c4212382460a69a0f7aa14c3
fslyg <43489679+fslyg@users.noreply.github.com>
2019-07-13 23:04:25 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,129 +1,224 @@
-#! /bin/bash
-if [[ -z "${UUID}" ]]; then
-  UUID="4890bd47-5180-4b1c-9a5d-3ef686543112"
-fi
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+#=================================================================#
+#   System Required: CentOS 7 X86_64                              #
+#   Description: Caddy + v2Ray + TLS + WebSocket Soft Install     #
+#   Author: LALA <QQ1062951199>                                   #
+#   Website: https://www.lala.im                                  #
+#=================================================================#
 
-if [[ -z "${AlterID}" ]]; then
-  AlterID="10"
-fi
+# 颜色选择
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+font="\033[0m"
 
-if [[ -z "${V2_Path}" ]]; then
-  V2_Path="/FreeApp"
-fi
+v2ray_install(){
 
-if [[ -z "${V2_QR_Path}" ]]; then
-  V2_QR_Code="1234"
-fi
+	# 变量
+	read -p "输入你的域名 (不要带http://):" Domain
+	read -p "输入你的邮箱 (用于自动签发SSL证书):" Email
+	read -p "输入Caddy监听端口 (建议443):" Port
 
-rm -rf /etc/localtime
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-date -R
+	# 关闭SELinux
+	if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
+		sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+		setenforce 0
+	fi
 
-SYS_Bit="$(getconf LONG_BIT)"
-[[ "$SYS_Bit" == '32' ]] && BitVer='_linux_386.tar.gz'
-[[ "$SYS_Bit" == '64' ]] && BitVer='_linux_amd64.tar.gz'
+	# 关闭防火墙
+	systemctl disable firewalld.service
+	systemctl stop firewalld.service
 
-if [ "$VER" = "latest" ]; then
-  V_VER=`wget -qO- "https://api.github.com/repos/v2ray/v2ray-core/releases/latest" | grep 'tag_name' | cut -d\" -f4`
-else
-  V_VER="v$VER"
-fi
+	# 某些奇葩机器C7可能还带iptables
+	systemctl disable iptables.service
+	systemctl stop iptables.service
 
-mkdir /v2raybin
-cd /v2raybin
-wget --no-check-certificate -qO 'v2ray.zip' "https://github.com/v2ray/v2ray-core/releases/download/$V_VER/v2ray-linux-$SYS_Bit.zip"
-unzip v2ray.zip
-rm -rf v2ray.zip
-chmod +x /v2raybin/v2ray-$V_VER-linux-$SYS_Bit/*
+	# 生成UUID
+	uuid=$(cat /proc/sys/kernel/random/uuid)
 
-C_VER=`wget -qO- "https://api.github.com/repos/mholt/caddy/releases/latest" | grep 'tag_name' | cut -d\" -f4`
-mkdir /caddybin
-cd /caddybin
-wget --no-check-certificate -qO 'caddy.tar.gz' "https://github.com/mholt/caddy/releases/download/$C_VER/caddy_$C_VER$BitVer"
-tar xvf caddy.tar.gz
-rm -rf caddy.tar.gz
-chmod +x caddy
-cd /root
-mkdir /wwwroot
-cd /wwwroot
+	# 安装Curl
+	yum -y install curl
 
-wget --no-check-certificate -qO 'demo.tar.gz' "https://github.com/ki8852/v2ray-heroku-undone/raw/master/demo.tar.gz"
-tar xvf demo.tar.gz
-rm -rf demo.tar.gz
+	# 安装V2ray
+	bash <(curl -L -s https://install.direct/go.sh)
 
-cat <<-EOF > /v2raybin/v2ray-$V_VER-linux-$SYS_Bit/config.json
+	# 写入配置文件
+	cat > /etc/v2ray/config.json <<EOF
 {
-    "log":{
-        "loglevel":"warning"
-    },
-    "inbound":{
-        "protocol":"vmess",
-        "listen":"127.0.0.1",
-        "port":2333,
-        "settings":{
-            "clients":[
-                {
-                    "id":"${UUID}",
-                    "level":1,
-                    "alterId":${AlterID}
-                }
-            ]
-        },
-        "streamSettings":{
-            "network":"ws",
-            "wsSettings":{
-                "path":"${V2_Path}"
-            }
+  "inbounds": [
+    {
+      "port": 10000,
+      "listen":"127.0.0.1",
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "${uuid}",
+            "alterId": 64
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws",
+        "wsSettings": {
+        "path": "/imlala"
         }
-    },
-    "outbound":{
-        "protocol":"freedom",
-        "settings":{
-        }
+      }
     }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    }
+  ]
 }
 EOF
 
-cat <<-EOF > /caddybin/Caddyfile
-http://0.0.0.0:${PORT}
-{
-	root /wwwroot
-	index index.html
-	timeouts none
-	proxy ${V2_Path} localhost:2333 {
+	# 安装并配置Caddy
+	cd ~
+	curl https://getcaddy.com | bash -s personal http.filebrowser
+	if [ $? -eq 0 ];then
+	    echo -e "${green} Caddy安装完成 ${font}"
+	else 
+	    echo -e "${red} Caddy安装失败 ${font}"
+	    exit 1
+	fi
+
+	mkdir -p /etc/caddy && mkdir -p /etc/ssl/caddy
+
+	read -p "是否需要把站点伪装成一个网盘?如选择伪装,在安装完成之后请访问${Domain}:${Port}修改管理员账号密码,默认均为:admin(yes/no):" mask
+	if [ $mask = "yes" ];then
+	cat > /etc/caddy/Caddyfile <<EOF
+${Domain}:${Port} {
+	log stdout
+	tls ${Email}
+	filebrowser / /opt
+	proxy /imlala localhost:10000 {
 		websocket
 		header_upstream -Origin
 	}
 }
 EOF
-
-cat <<-EOF > /v2raybin/vmess.json 
-{
-    "v": "2",
-    "ps": "${AppName}.herokuapp.com",
-    "add": "${AppName}.herokuapp.com",
-    "port": "443",
-    "id": "${UUID}",
-    "aid": "${AlterID}",			
-    "net": "ws",			
-    "type": "none",			
-    "host": "",			
-    "path": "${V2_Path}",	
-    "tls": "tls"			
+	fi
+	if [ $mask = "no" ];then
+	cat > /etc/caddy/Caddyfile <<EOF
+${Domain}:${Port} {
+	log stdout
+	tls ${Email}
+	proxy /imlala localhost:10000 {
+		websocket
+		header_upstream -Origin
+	}
 }
 EOF
+	fi
 
-if [ "$AppName" = "no" ]; then
-  echo "不生成二维码"
-else
-  mkdir /wwwroot/$V2_QR_Path
-  vmess="vmess://$(cat /v2raybin/vmess.json | base64 -w 0)" 
-  Linkbase64=$(echo -n "${vmess}" | tr -d '\n' | base64 -w 0) 
-  echo "${Linkbase64}" | tr -d '\n' > /wwwroot/$V2_QR_Path/index.html
-  echo -n "${vmess}" | qrencode -s 6 -o /wwwroot/$V2_QR_Path/v2.png
-fi
+	#创建Caddy服务文件
+	cat > /etc/systemd/system/caddy.service <<EOF
+[Unit]
+Description=Caddy HTTP/2 web server
+Documentation=https://caddyserver.com/docs
+After=network-online.target
+Wants=network-online.target systemd-networkd-wait-online.service
 
-cd /v2raybin/v2ray-$V_VER-linux-$SYS_Bit
-./v2ray &
-cd /caddybin
-./caddy -conf="Caddyfile"
+[Service]
+Restart=on-abnormal
+User=root
+Group=root
+Environment=CADDYPATH=/etc/ssl/caddy
+ExecStart=/usr/local/bin/caddy -log stdout -agree=true -conf=/etc/caddy/Caddyfile
+ExecReload=/bin/kill -USR1 \$MAINPID
+KillMode=mixed
+KillSignal=SIGQUIT
+TimeoutStopSec=5s
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+	#启动Caddy
+	systemctl enable caddy
+	systemctl start caddy
+
+	#启动v2Ray
+	systemctl start v2ray
+	systemctl enable v2ray
+
+	# 打印客户端配置信息
+	clear
+	echo -e "${green} 安装完成 ${font}"
+	echo -e "${yellow} 客户端连接地址(Address) : ${Domain} ${font}"
+	echo -e "${yellow} 端口(Port) : ${Port} ${font}"
+	echo -e "${yellow} 用户ID(UUID) : ${uuid} ${font}"
+	echo -e "${yellow} 额外ID(Alterid) : 64 ${font}"
+	echo -e "${yellow} 传输协议(Network) : WS ${font}"
+	echo -e "${yellow} 路径(Path) : /imlala ${font}"
+}
+
+kernelpcc_install(){
+
+	# 更新系统
+	yum -y update
+
+	# 安装依赖
+	yum -y install kernel-headers-$(uname -r) kernel-devel-$(uname -r)
+	yum -y install centos-release-scl
+	yum -y groupinstall "Development Tools"
+	yum -y install devtoolset-7-gcc*
+
+	# 下载源码
+	cd ~
+	git clone https://github.com/giltu/KernelPCC.git
+	cd KernelPCC
+
+	# 修复编译内核的路径和版本
+	cat > Makefile <<EOF
+obj-m += tcp_TA.o
+KVERSION := /usr/src/kernels/\$(shell uname -r)	
+PWD := \$(shell pwd)
+default:
+	make -C \$(KVERSION) SUBDIRS=\$(PWD) modules
+clean:
+	make -C \$(KVERSION) M=\$(PWD) clean
+EOF
+
+	# 使用高版本GCC编译
+	scl enable devtoolset-7 make
+
+	# 加载模块到内核
+	insmod tcp_TA.ko
+
+	# 启用模块
+	echo "net.ipv4.tcp_congestion_control=TA" >> /etc/sysctl.conf
+	sysctl -p
+
+	# 开机启动
+	echo "insmod ~/KernelPCC/tcp_TA.ko" >> /etc/rc.d/rc.local
+	chmod +x /etc/rc.d/rc.local
+
+	# 清屏打印输出
+	clear
+	echo -e "${green} 安装完成请执行命令查看是否启用成功: sysctl net.ipv4.tcp_congestion_control ${font}"
+	echo -e "${green} 如输出结果=号后面有TA字样则说明成功 ${font}"
+}
+
+# 开始菜单设置
+echo -e "${yellow} CentOS7 X86_64 Caddy + v2Ray + TLS + WebSocket 一键安装脚本 ${font}"
+echo -e "${yellow} Author LALA Website WWW.LALA.IM ${font}"
+start_menu(){
+	read -p "请输入数字(1或2),1安装v2Ray,2安装KernelPCC(类似BBR的单边加速):" num
+	case "$num" in
+		1)
+		v2ray_install
+		;;
+		2)
+		kernelpcc_install
+		;;
+	esac
+}
+
+# 运行开始菜单
+start_menu
